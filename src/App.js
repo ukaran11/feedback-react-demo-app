c01439ff4a136d8cbc464364de8c53355aa93fe1
unknown <ukaran11@gmail.com>
2022-04-15 16:25:40 +0530

    Utsav | Added Header comp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>App</div>
+      <>
+        <Header/>
+        <div>App</div>
+      </>
   )
 }
 
