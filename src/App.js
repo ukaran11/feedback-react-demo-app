69adffc9cbfcd76ae2e319ce1a7152d7641c9490
unknown <ukaran11@gmail.com>
2022-04-15 17:54:04 +0530

    Utsav | Added FeedbackItem comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import FeedbackItem from './components/FeedbackItem'
 import Header from './components/Header'
 
 const App = () => {
   return (
       <>
         <Header/>
-        <div>App</div>
+        <div className='container'>
+            <FeedbackItem />
+        </div>
       </>
   )
 }
