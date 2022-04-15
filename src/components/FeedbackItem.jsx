ccbbb7e9595ee36a6873fa3a71825a77b84b273a
unknown <ukaran11@gmail.com>
2022-04-16 01:18:19 +0530

    Utsav | Added close button

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,14 @@
-import Card from "./shared/Card"
+import { FaTimes } from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import Card from "./shared/Card"
 
 const FeedbackItem = ({item}) => {
   return (
     <Card>
         <div className="num-display">{item.rating}</div>
+        <button className="close">
+          <FaTimes color="purple"/>
+        </button>
         <div className="text-display">{item.text}</div>
     </Card>
   )
