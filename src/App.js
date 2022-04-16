8f15b8e53efb300c615c89623cf48c96fa0e5dec
unknown <ukaran11@gmail.com>
2022-04-17 03:36:41 +0530

    Utsav | Added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { v4 as uuidv4} from 'uuid';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FeedbackForm from './components/FeedbackForm'
 import FeedbackItem from './components/FeedbackItem'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import Header from './components/Header'
 import FeedbackData from './data/FeedbackData'
+import AboutPage from './pages/AboutPage';
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -23,14 +24,22 @@ const App = () => {
   }
 
   return (
-      <>
+      <Router>
         <Header/>
         <div className='container'>
-            <FeedbackForm handleAdd={addFeedback}/>
-            <FeedbackStats feedback={feedback}/>
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+          <Routes>
+            <Route exact path='/' element={
+              <>
+              <FeedbackForm handleAdd={addFeedback}/>
+              <FeedbackStats feedback={feedback}/>
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+              </>
+            }>
+            </Route>
+            <Route path='/about' element={<AboutPage />}/>
+        </Routes>
         </div>
-      </>
+      </Router>
   )
 }
 
